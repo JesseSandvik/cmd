ca7a9594737dc59ccfbd68d40d5e45960409515d
Jesse Sandvik <sandvikjesse@gmail.com>
2023-12-04 21:49:19 -0500

    cleaned project, beginning gherkin base command

--- src/main/java/com/blckroot/cmd/PluginCommandContract.java
+++
@@ -1,9 +0,0 @@
-package com.blckroot.cmd;
-
-import java.util.List;
-
-interface PluginCommandContract extends CommandContract {
-    String getExecutableFileDirectory();
-    List<PluginCommand> getPluginSubcommands();
-    void addPluginSubcommand(PluginCommand executableSubcommand);
-}
